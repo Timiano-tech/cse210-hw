1d499c73f262f845aebc75fbea6766d333448db6
Timiano <timiajayi729@gmail.com>
2026-07-08 18:54:28 +0100

    week 03 adjustment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,36 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask the user for the magic number
+        // Ask for magic number
         Console.Write("What is the magic number? ");
+
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        // Start guessing loop
 
+        int guess = -1;
 
-        if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (guess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
-
-
-        Console.WriteLine("\n--- Now with a loop! ---");
-
-
-        Console.Write("What is the magic number? ");
-        magicNumber = int.Parse(Console.ReadLine());
-
-        // Start the loop
-        guess = -1;
 
         while (guess != magicNumber)
         {
@@ -54,34 +33,6 @@ class Program
             }
         }
 
-        Console.WriteLine("\n--- Now with a random number! ---");
-
-        // Create a random number generator
-        Random randomGenerator = new Random();
-        magicNumber = randomGenerator.Next(1, 101); // 1 to 100
-
-        // Reset the guess
-        guess = -1;
-
-        // Play the game with the random number
-        while (guess != magicNumber)
-        {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
 
         Console.WriteLine("Thanks for playing!");
     }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,66 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a list to store the numbers
-        List<int> numbers = new List<int>();
 
-        Console.WriteLine("Enter a series of numbers (enter 0 to stop):");
-
-        int userNumber = -1;
-
-        while (userNumber != 0)
+        for (int i = 0; i < 50; i++)
         {
-            Console.Write("Enter a number: ");
-            userNumber = int.Parse(Console.ReadLine());
-
-
-            if (userNumber != 0)
+            Console.WriteLine(i);
+            if (i == 20)
             {
-                numbers.Add(userNumber);
+                Console.WriteLine("The loop is stopping at 20");
+                break;
             }
         }
 
-        if (numbers.Count > 0)
-        {
-            // Calculate the sum
-            int sum = 0;
-            foreach (int number in numbers)
-            {
-                sum = sum + number;
-            }
-
-
-            float average = (float)sum / numbers.Count;
-
-            int max = numbers[0];
-            foreach (int number in numbers)
-            {
-                if (number > max)
-                {
-                    max = number;
-                }
-            }
-
-            // Display the results
-            Console.WriteLine();
-            Console.WriteLine("The numbers you entered were:");
-            foreach (int number in numbers)
-            {
-                Console.Write(number + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("The sum is: " + sum);
-            Console.WriteLine("The average is: " + average);
-            Console.WriteLine("The largest number is: " + max);
-        }
-        else
-        {
-            Console.WriteLine("You didn't enter any numbers!");
-        }
-
-        Console.WriteLine();
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
     }
 }
